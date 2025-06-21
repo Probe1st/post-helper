4b0ca6b2342505feb9ad9a9a138b54937fd3cc91
Probe1st <svintsitskiy.leha@mail.ru>
2025-06-22 04:02:18 +0500

    created forwarding messages

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-import bot from "./src/bot";
+import bot from "./src/bot.js";
+import { createPhotoIdsStorage, deletePhotoId, getPhotoIds, getPhotoIdsCount, savePhotoId } from "./src/utils/photoStorage.js";
+
+createPhotoIdsStorage();
 
 bot.start();
--- src/bot.js
+++ src/bot.js
@@ -1,5 +1,10 @@
-const { Bot } = require("grammy");
-
+import { Bot, session } from "grammy";
+import { mainScene } from "./scenes/mainScene/scene.js";
+import {
+    deletePhotoId,
+    getPhotoIds,
+    savePhotoId,
+} from "./utils/photoStorage.js";
 
 const bot = new Bot(process.env.BOT_TOKEN);
 
@@ -8,10 +13,44 @@ bot.use(
         initial: () => ({
             step: 1,
         }),
-    })
+    }),
 );
 
-bot.command("start", async ctx => await mainScene(ctx));
+// check if user is owner
+bot.use(async (ctx, next) => {
+    if (ctx?.from?.id == process.env.OWNER_ID) {
+        await next();
+    }
+});
+
+bot.command("start", async (ctx) => await mainScene(ctx));
+
+bot.on("message:photo", async (ctx) => {
+    const photoId = ctx.message.photo.at(-1).file_id;
+    savePhotoId(photoId);
+
+    let countPosts = Math.floor(getPhotoIds().length / 10);
+
+    for (let i = 0; i < countPosts; i++) {
+        const sendPhotoIds = getPhotoIds()
+            .slice(0, getPhotoIds().length > 10 ? 10 : undefined)
+            .map((photoId) => ({ type: "photo", media: photoId }));
+        
+        console.log(sendPhotoIds)
+
+        sendPhotoIds.forEach((photoId) => {
+            deletePhotoId(photoId.media);
+        });
+
+        sendPhotoIds.length != 0 &&
+            (await bot.api.sendMediaGroup(
+                process.env.TARGET_CHANNEL_ID,
+                sendPhotoIds,
+            ));
+    }
+
+    ctx.reply("media is saved");
+});
 
 bot.on("message");
 
